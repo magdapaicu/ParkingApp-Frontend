26602d2185774d6c725d07624fffcfe1d1667717
PaicuMagda <paicumagda962@gmail.com>
2023-09-28 11:32:19 +0300

    alt commit de incercare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewComponent } from './new/new.component';
+import { New2Component } from './new2/new2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewComponent
+    NewComponent,
+    New2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/new2/new2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new2',
+  templateUrl: './new2.component.html',
+  styleUrls: ['./new2.component.scss']
+})
+export class New2Component {
+
+}
